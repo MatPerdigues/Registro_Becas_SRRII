1c8759f15c4e078da1453cf2cf0dfa2b4d3688ce
MatPerdigues <matperdigues@gmail.com>
2023-10-18 03:08:01 -0300

    V1.0

---
+++ FRONT/src/components/route/route.js
@@ -0,0 +1,13 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+export default function Ruteo() {
+return(
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    
+    </BrowserRouter>
+)
+
+}
