20a88cb61aee0b9efa54c6354f0637058c1df438
MatPerdigues <matperdigues@gmail.com>
2023-10-19 01:08:37 -0300

    V_19Oct23

--- FRONT/src/components/route/route.js
+++ FRONT/src/components/route/route.js
@@ -1,10 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from '../home/home'
 
 export default function Ruteo() {
 return(
     <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path='/' element={<Home/>}></Route>
         </Routes>
     
     </BrowserRouter>
