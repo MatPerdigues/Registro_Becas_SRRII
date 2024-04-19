d7e2a79ec75e807beb4d27ffdc0774ea5842105a
MatPerdigues <matperdigues@gmail.com>
2024-04-19 02:45:30 -0300

    V.18Abr24-1-

--- API/config/dataBase.js
+++ API/config/dataBase.js
@@ -8,18 +8,35 @@ const dataBaseInfo = {
     database: process.env.DB,
     password: process.env.PASS,
     connectTimeout : 1000000,
-    waitForConnections : true
+    waitForConnections : true,
+    connectionLimit : 10,
     
 };
-
+/*
 const dbConnection = mysql.createConnection(dataBaseInfo);
 
-dbConnection.connect((error)=>{
+ dbConnection.connect((error)=>{
     if(error){
         console.log(error)
     } else {
         console.log('Conexión con DB MySQL exitosa')
     }
-});
+}); */
+
+const dbConnection = mysql.createPool(dataBaseInfo)
+
+dbConnection.getConnection((error,connect)=>{
+    if (error) {
+        connect.release();
+        throw err;
+    }else{
+        console.log('Conexión con DB MySQL exitosa')
+    }   
+      
+})
+
+
+
+
 
 module.exports=dbConnection;
